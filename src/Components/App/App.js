39c7fb1556b306c37a0f976cd2993699c95749d7
Denzel Turambi <dturambi26@gmail.com>
2023-09-20 15:31:44 -0600

    feat: set beers to api data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getBeers } from '../ApiCalls';
 
 function App() {
+  const [beers, setBeers] = useState([])
+
+  useEffect(() => {
+    getBeers()
+    .then(data => setBeers(data))
+  }, []);
+
+  console.log(beers)
+
   return (
     <h1>Brew Buddy</h1>
   );
