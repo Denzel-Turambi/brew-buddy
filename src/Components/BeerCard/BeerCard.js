6ad68aeb38877389bfd1b15b9e006f38694178c6
Denzel Turambi <dturambi26@gmail.com>
2023-09-20 17:56:03 -0600

    feat: Add hover state to card

--- src/Components/BeerCard/BeerCard.js
+++ src/Components/BeerCard/BeerCard.js
@@ -3,8 +3,8 @@ import './BeerCard.css'
 function BeerCard({ name, id, img }) {
   return (
     <div className='card' id={id}>
-      <img className ='beer-img' src={img} alt='beer image'/>
-      <h1>{name}</h1>
+      <img className ='beer-img' id={id} src={img} alt='beer image'/>
+      <h1 id={id}>{name}</h1>
     </div>
   )
 }
