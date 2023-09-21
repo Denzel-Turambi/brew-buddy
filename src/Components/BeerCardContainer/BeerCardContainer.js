eb8d313c6485c50e54793f8f132229395adb2e27
Denzel Turambi <dturambi26@gmail.com>
2023-09-21 14:32:12 -0600

    refactor: Add conditional rendering to show all beers or filtered beers

--- src/Components/BeerCardContainer/BeerCardContainer.js
+++ src/Components/BeerCardContainer/BeerCardContainer.js
@@ -1,23 +1,37 @@
 import './BeerCardContainer.css'
 import BeerCard from '../BeerCard/BeerCard'
 
-function BeerCardContainer({beers}) {
-  const beerCards = beers.map(beer => {
+function BeerCardContainer({ beers, search, filter }) {
+  let allBeers = [];
+  let filtered = [];
+
+  filtered = search ? filter.map(filteredBeer => {
     return (
-      <BeerCard 
-        name={beer.name}
-        id={beer.id}
-        key={beer.id}
-        img={beer.image_url}
+      <BeerCard
+        name={filteredBeer.name}
+        id={filteredBeer.id}
+        key={filteredBeer.id}
+        img={filteredBeer.image_url}
       />
     )
-  })
+  }) : (
+    allBeers = beers.map(beer => {
+      return (
+        <BeerCard
+          name={beer.name}
+          id={beer.id}
+          key={beer.id}
+          img={beer.image_url}
+        />
+      )
+    })
+  )
 
-  return(
-  <div className='beer-container'>
-    {beerCards}
-  </div>
- )
+  return (
+    <div className='beer-container'>
+      {search ? filtered : allBeers}
+    </div>
+  )
 }
 
 export default BeerCardContainer;
