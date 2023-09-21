bfe1430ecf29796b0feb1059c42000d59e0721a0
Denzel Turambi <dturambi26@gmail.com>
2023-09-20 19:46:41 -0600

    feat: Link card to single beer/focus component

--- src/Components/BeerCard/BeerCard.js
+++ src/Components/BeerCard/BeerCard.js
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import './BeerCard.css'
 
 function BeerCard({ name, id, img }) {
   return (
-    <div className='card' id={id}>
-      <img className ='beer-img' id={id} src={img} alt='beer image'/>
-      <h1 id={id}>{name}</h1>
-    </div>
+    <Link to={`/${id}`} style={{textDecoration:'none'}}>
+      <div className='card' id={id}>
+        <img className ='beer-img' id={id} src={img} alt='beer image'/>
+        <h1 id={id}>{name}</h1>
+      </div>
+    </Link>
   )
 }
 
--- src/Components/Focus/Focus.js
+++ src/Components/Focus/Focus.js
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
 import './Focus.css'
+import { useParams } from 'react-router-dom';
+import { getSingleBeer } from '../ApiCalls';
 
 function Focus() {
+  const id = useParams()
+  const [singleBeer, setSingleBeer] = useState([]);
+
+  useEffect(() => {
+    getSingleBeer(id.id)
+    .then(data => setSingleBeer(data[0]))
+  }, []);
+
+  console.log(singleBeer)
+
   return
 }
 
