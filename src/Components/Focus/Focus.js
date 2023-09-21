e918f37a31f5f1c93f460bfac257ac618164a744
Denzel Turambi <dturambi26@gmail.com>
2023-09-20 19:59:31 -0600

    feat: Display single beer info

--- src/Components/Focus/Focus.js
+++ src/Components/Focus/Focus.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import { getSingleBeer } from '../ApiCalls';
 
 function Focus() {
-  const id = useParams()
+  const id = useParams();
   const [singleBeer, setSingleBeer] = useState([]);
 
   useEffect(() => {
@@ -14,7 +14,22 @@ function Focus() {
 
   console.log(singleBeer)
 
-  return
+  return(
+    <div className="single-beer-container">
+      <div className="single-beer-icon">
+        <img className="single-beer-img" src={singleBeer.image_url}/>
+      </div>
+      <div className="single-beer-description">
+        <h1>{singleBeer.name}</h1>
+        <h2>{singleBeer.tagline}</h2>
+        <p><strong>First Brewed: </strong>{singleBeer.first_brewed}</p>
+        <p><strong>Description: </strong>{singleBeer.description}</p>
+        <p><strong>Alcohol by volume (ABV): </strong>{singleBeer.abv}</p>
+        <p><strong>Food Pairings: </strong>{singleBeer.food_pairing}</p>
+        <p><strong>Brewers Tips: </strong>{singleBeer.brewers_tips}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Focus;
