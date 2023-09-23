ac7ac3c4994f4b788e118994930a341c13e8a85a
Denzel Turambi <dturambi26@gmail.com>
2023-09-22 20:05:03 -0600

    feat: Add error handling for 500 error status

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,26 +5,29 @@ import NavBar from '../NavBar/NavBar';
 import BeerCardContainer from '../BeerCardContainer/BeerCardContainer';
 import { Routes, Route } from 'react-router-dom';
 import Focus from '../Focus/Focus';
+import Error500 from '../ErrorHandling/Error500';
 
 function App() {
   const [beers, setBeers] = useState([]);
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("");
   const [searchValue, setSearchValue] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     getBeers()
     .then(data => setBeers(data))
+    .catch(error => setError(error))
   }, []);
 
   useEffect(() => {
     const filteredBeers = beers.filter(beer => 
       beer.name.toLowerCase().includes(search)
-    );
-    console.log('Filtered Beers', filteredBeers)
-    setFilter(filteredBeers)
-  }, [beers, search])
-
+      );
+      console.log('Filtered Beers', filteredBeers)
+      setFilter(filteredBeers)
+    }, [beers, search])
+    
   function searchFilter(event) {
     setSearchValue(event.target.value);
     const searchBeer = event.target.value.toLowerCase();
@@ -34,6 +37,7 @@ function App() {
   return (
     <div>
     <NavBar search={search} searchFilter={searchFilter}/>
+    <Error500 error={error}/>
     <Routes>
       <Route path='/' element={
         <BeerCardContainer beers={beers} search={search} filter={filter}/>
