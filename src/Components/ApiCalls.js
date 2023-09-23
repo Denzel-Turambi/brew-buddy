5297a808328ec9fa4289d57b0f6dfc7d9769b24b
Denzel Turambi <dturambi26@gmail.com>
2023-09-23 16:58:36 -0600

    refactor: Fix API url

--- src/Components/ApiCalls.js
+++ src/Components/ApiCalls.js
@@ -1,5 +1,5 @@
 function getBeers() {
-  return fetch('https://api.punkapi.com/v2/beersfff')
+  return fetch('https://api.punkapi.com/v2/beers')
   .then(res => {
     if(!res.ok){
       throw new Error (`${res.status}`)
